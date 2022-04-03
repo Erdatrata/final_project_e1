789257735c621dbe8c91397a64a399b4342ea718
Erdat rata <yosirat2016@gmail.com>
2022-04-03 22:06:29 +0300

    Upload_pic

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,40 +1,87 @@
 import React, {useEffect, useState} from 'react';
-import {
-  View,
-  ScrollView,
-  Text,
-  StyleSheet,
-  FlatList,
-  SafeAreaView,
-  Alert,
-} from 'react-native';
+import {View,Text,StyleSheet,FlatList} from 'react-native';
 
-import {Container} from '../styles/FeedStyles';
-import { useContext } from 'react/cjs/react.development';
-import { AuthContext } from '../navigation/AuthProvider';
-import FormButton from '../components/FormButton';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import PostCrad from '../components/Postcard';
+import {
+  Container,
+ 
+} from '../styles/FeedStyles';
+
+const Posts = [
+  {
+    id: '1',
+    userName: 'Jenny Doe',
+    userImg: require('../assets/users/user-3.jpg'),
+    postTime: '4 mins ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: require('../assets/posts/post-img-3.jpg'),
+    liked: true,
+    likes: '14',
+    comments: '5',
+  },
+  {
+    id: '2',
+    userName: 'John Doe',
+    userImg: require('../assets/users/user-1.jpg'),
+    postTime: '2 hours ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: 'none',
+    liked: false,
+    likes: '8',
+    comments: '0',
+  },
+  {
+    id: '3',
+    userName: 'Ken William',
+    userImg: require('../assets/users/user-4.jpg'),
+    postTime: '1 hours ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: require('../assets/posts/post-img-2.jpg'),
+    liked: true,
+    likes: '1',
+    comments: '0',
+  },
+  {
+    id: '4',
+    userName: 'Selina Paul',
+    userImg: require('../assets/users/user-6.jpg'),
+    postTime: '1 day ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: require('../assets/posts/post-img-4.jpg'),
+    liked: true,
+    likes: '22',
+    comments: '4',
+  },
+  {
+    id: '5',
+    userName: 'Christy Alex',
+    userImg: require('../assets/users/user-7.jpg'),
+    postTime: '2 days ago',
+    post:
+      'Hey there, this is my test for a post of my social app in React Native.',
+    postImg: 'none',
+    liked: false,
+    likes: '0',
+    comments: '0',
+  },
+];
 
 const HomeScreen = () =>{
-    const {user,logout}=useContext(AuthContext);
     return (
-        <View style={styles.Container}>
-        <Text style={styles.text}>Welcome{user.uid}</Text>
-        <FormButton buttonTitle='Logout'   onPress={() => logout()} />
-        </View>
+       <Container>
+         <FlatList
+         data={Posts}
+         renderItem={({item})=> <PostCrad item={item}/>}
+         keyExtractor={item=>item.id}
+         showsVerticalScrollIndicator={false}
+         />
+       </Container>
     );
 }
 export default HomeScreen;
-const styles =StyleSheet.create({
-    Container:{
-        backgroundColor:'#f9fafd',
-        flex:1,
-        justifyContent:'center',
-        alignnItems:'center',
-        padding:20,
-
-    },
-    text:{
-        fontSize:20,
-        color:'#333333'
-    }
-});
