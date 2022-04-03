14af8a1c3339dc733ffe3f1c7c923d1573a952ad
Erdat rata <yosirat2016@gmail.com>
2022-04-03 22:05:56 +0300

    main_activity

---
+++ screens/AddPostScreen.js
@@ -0,0 +1,54 @@
+import React, {useState, useContext} from 'react';
+import {
+  View,
+  Text,
+  Platform,
+  StyleSheet,
+  Alert,
+  ActivityIndicator,
+  Button,
+} from 'react-native';
+// import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
+import ImagePicker from 'react-native-image-crop-picker';
+
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
+
+import {
+  InputField,
+  InputWrapper,
+  AddImage,
+  SubmitBtn,
+  SubmitBtnText,
+  StatusWrapper,
+} from '../styles/AddPost';
+
+import { AuthContext } from '../navigation/AuthProvider';
+
+const AddPostScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Add Post Screen</Text>
+      <Button
+      title="Click Here"
+      onPress={()=> alert('Button Clicked')}
+      />
+    </View>
+  );
+};
+
+export default AddPostScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
