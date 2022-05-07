f8e08d3d457840b408b3535f9c98860d93163609
Erdat rata <yosirat2016@gmail.com>
2022-05-07 21:10:00 +0300

    all_prblem_fix

--- navigation/index.js
+++ navigation/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Routes from './Routes';
 import { AuthProvider } from './AuthProvider';
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  Alert,
+  RefreshControl, 
+ 
+} from 'react-native';
 const Providers = () => {
   return (
     <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <Routes />
+  </AuthProvider>
   );
 }
 
