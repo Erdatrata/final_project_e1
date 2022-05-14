92bb056d7fa27b7bb970f474c2d4fb45362356bc
Erdat rata <yosirat2016@gmail.com>
2022-05-15 02:45:53 +0300

    fix_google_map

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,86 +1,199 @@
-import React from 'react';
-import MapView,{PROVIDER_GOOGLE,Marker, Callout} from 'react-native-maps';
-import {View,Text,Button,StyleSheet} from 'react-native';
+//  import React, {useState, useEffect} from 'react';
+// import {StyleSheet, Text} from 'react-native';
+// import MapView, {Marker} from 'react-native-maps';
+// import Geolocation from '@react-native-community/geolocation';
+// import getDirections from 'react-native-google-maps-directions'
+// const App = () => {
+//   const [position, setPosition] = useState({
+//     latitude: 22.62938671242907,
+//     longitude: 88.4354486029795,
+//     latitudeDelta: 0.04864195044303443,
+//     longitudeDelta: 0.040142817690068,
+//   });
 
-const ExploreScreen =()=>{
-return(
-  <MapView
-  provider={ PROVIDER_GOOGLE} 
-  style={styles.map}
-  region={{
-    latitude: 22.62938671242907,
-    longitude: 88.4354486029795,
-    latitudeDelta: 0.04864195044303443,
-    longitudeDelta: 0.040142817690068,
-  }}
-  >
-    <Marker
-    coordinate={{
-      latitude: 22.62938671242907,
-      longitude: 88.4354486029795,
-      latitudeDelta: 0.04864195044303443,
-      longitudeDelta: 0.040142817690068,
-    }}
-    Image={require('../assets/map_marker.png')}
-    title="Test Title"
-    description="This is decrption"
-    />
-    {/* <Callout tooltip>
-    < View style={styles.bubble}>
-    <Text style={styles.name}>
-      goof place
-    </Text>
-    <View style ={styles.arrowBorder}/>
-    <View style ={styles.arrow}/>
-     </View>   
-    </Callout> */}
-  </MapView>
+  // useEffect(() => {
+  //   Geolocation.getCurrentPosition((pos) => {
+  //     const crd = pos.coords;
+  //     setPosition({
+  //       latitude: crd.latitude,
+  //       longitude: crd.longitude,
+  //       latitudeDelta: 0.0421,
+  //       longitudeDelta: 0.0421,
+  //     });
+  //   }).catch((err) => {
+  //     console.log(err);
+  //   });
+  // }, []);
+
+//   return (
+//     <MapView
+//       style={styles.map}
+//       initialRegion={position}
+//       showsUserLocation={true}
+//       showsMyLocationButton={true}
+//       followsUserLocation={true}
+//       showsCompass={true}
+//       scrollEnabled={true}
+//       zoomEnabled={true}
+//       pitchEnabled={true}
+//       rotateEnabled={true}>
+//        <Marker
+//        title='Yor are here'
+//       //  description='This is a description'
+//        coordinate={position}/>
+//        </MapView>
+//   );
+// };
 
 
-);
 
-};
-export default ExploreScreen;
-const styles = StyleSheet.create({
-map:{
-height:'100%'
-},
-container:{
-flexDirection:'column',
-alignSelf:'flex-start',
-},
-bubble:{
-flaxDirection:'row',
-alignSelf:'flex-start',
-backgroundColor:'#fff',
-borderRadius:6,
-borderWidth:0.5,
-padding:15,
-width:150,
-},
-arrow:{
-backgroundColor:'transparens',
-borderColor:'transparens',
-borderTopColor:'#fff',
-borderWidth:16,
-alignSelf:'center',
-marginTop:-32,
-},
-arrowBorder:{
-  backgroundColor:'transparens',
-  borderColor:'transparens',
-  borderTopColor:'#007a87',
-  borderWidth:16,
-  alignSelf:'center',
-  marginTop:-0.5,
-},
-name:{
-  fontSize:16,
-  marginBottom:5,
-},
-Image:{
-width:120,
-height:80,
+// export default App;
+// const styles = StyleSheet.create({
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+// });
+
+import React, { Component } from 'react';
+import { StyleSheet, 
+	Button, 
+	Text, 
+	View, 
+	TextInput, 
+	ImageBackground, 
+	ActivityIndicator, 
+	Linking, 
+	TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+// import getDirections from 'react-native-google-maps-directions'
+
+
+import flagBlueImg from '../assets/map_marker.png';
+const isValidLatLng = (num, range) => typeof num === 'number' && num <= range && num >= -1 * range
+
+const isValidCoordinates = coords =>
+  isValidLatLng(coords.latitude, 90) && isValidLatLng(coords.longitude, 180)
+
+const getParams = (params = []) => {
+  return params
+    .map(({ key, value }) => {
+      const encodedKey = encodeURIComponent(key)
+      const encodedValue = encodeURIComponent(value)
+      return `${encodedKey}=${encodedValue}`
+    })
+    .join('&')
 }
 
-});
+const getWaypoints = (waypoints = []) => {
+  if (waypoints.length === 0) {
+    return ''
+  }
+
+  const params = waypoints
+    .map(value => `${value.latitude},${value.longitude}`)
+    .join('|')
+
+  return `&waypoints=${params}`
+}
+
+function getDirections ({ destination, source, params = [], waypoints = [] } = {}) {
+  if (destination && isValidCoordinates(destination)) {
+    params.push({
+      key: 'destination',
+      value: `${destination.latitude},${destination.longitude}`
+    })
+  }
+
+  if (source && isValidCoordinates(source)) {
+    params.push({
+      key: 'origin',
+      value: `${source.latitude},${source.longitude}`
+    })
+  }
+
+  const url = `https://www.google.com/maps/dir/?api=1&${getParams(
+    params
+  )}${getWaypoints(waypoints)}`
+  return url
+}
+
+
+
+let Container = styled(View) `
+	width: 100%;
+	height: 100%;
+	background-color: white;
+`;
+
+let Map = styled(MapView) `
+	width: 100%;
+	height: 100%;
+`;
+
+const origin = { latitude: 31.74454030107937, longitude: 34.98723725663177 };
+const destination = { latitude:32.02041458492151, longitude: 34.7733842633785 };
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBz54UfZ7G9RYlXlkyQ_foh75y6fhMGtC8';
+
+export default class extends Component {
+
+	constructor() {
+		super();
+	}
+
+	handleGetDirections = () => {		
+		const data = {
+			destination,
+			params: [
+				{
+					key: "dirflg",
+					value: "d"
+				}
+			]
+		} 
+
+ 
+   return getDirections(data);
+	}
+
+	pressed(e) {
+		console.log('pressed');
+		
+	}
+
+	render() {
+		return (
+			<Container>
+				<Map
+					initialRegion={{
+				    latitude: 32.02065081278995,
+            longitude:34.771281264084394,
+            latitudeDelta:  32.02041458492151,
+            longitudeDelta:34.7733842633785,
+					}}
+				>
+					<MapView.Marker
+						coordinate={destination}
+						image={flagBlueImg}
+					>
+						<MapView.Callout onPress={() => {
+							console.log('Press to Get Direction');      
+              Linking.openURL(this.handleGetDirections());
+					    
+						}}>
+							<Text>Press to Get Direction</Text>
+						</MapView.Callout>
+					</MapView.Marker>
+					<MapViewDirections
+						origin={origin}
+						destination={destination}
+						apikey={GOOGLE_MAPS_APIKEY}
+					/>
+
+				</Map>
+			</Container>
+		);
+	}
+}
