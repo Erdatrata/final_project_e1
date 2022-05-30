e1d08d2300d88573ef8070d164a72bd3602eb41a
Erdat rata <yosirat2016@gmail.com>
2022-05-30 23:40:16 +0300

    save_music_function

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -2,12 +2,14 @@ import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
   Text,
+  ScrollView,
   StyleSheet,
   SafeAreaView,
   Dimensions,
   TouchableOpacity,
   Image,
   FlatList,
+  RefreshControl,
   Animated,
 } from 'react-native';
 
@@ -23,54 +25,138 @@ import TrackPlayer, {
 import Slider from '@react-native-community/slider';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import songs from '../model/data_1';
+// import songs from './data_1';
+import storage from '@react-native-firebase/storage';
 
 const {width, height} = Dimensions.get('window');
 
-const setupPlayer = async () => {
-  try {
-    await TrackPlayer.setupPlayer();
-    await TrackPlayer.updateOptions({
-      capabilities: [
-        Capability.Play,
-        Capability.Pause,
-        Capability.SkipToNext,
-        Capability.SkipToPrevious,
-        Capability.Stop,
-      ],
-    });
-    await TrackPlayer.add(songs);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const togglePlayBack = async playBackState => {
-  const currentTrack = await TrackPlayer.getCurrentTrack();
-  console.log(currentTrack, playBackState, State.Playing);
-  if (currentTrack != null) {
-    if (playBackState == State.Paused) {
-      await TrackPlayer.play();
-    } else {
-      await TrackPlayer.pause();
-    }
-  }
-};
 
 const MusicPlayer = () => {
+
+  // console.log("-----songs-----",songs.data_url())
+  // console.log("-----songs_1-----",songs.data_url_2())
   const playBackState = usePlaybackState();
   const progress = useProgress();
   //   custom states
+  const [posts, setPosts] = useState(null);
+  const [elems, setElems] = useState([]);
   const [songIndex, setsongIndex] = useState(0);
   const [repeatMode, setRepeatMode] = useState('off');
   const [trackTitle, setTrackTitle] = useState();
   const [trackArtist, setTrackArtist] = useState();
   const [trackArtwork, setTrackArtwork] = useState();
   // custom referecnces
+  // const [refreshing, setRefreshing] = React.useState(false);
+
   const scrollX = useRef(new Animated.Value(0)).current;
   const songSlider = useRef(null);
+  const songs = [
+    {
+      id: 1,
+      title: '19th Floor',
+      artist: 'Bobby Richards',
+      artwork: require('../assets/img/img1.jpg'),
+      url: 'https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FAudio%2F19th%20Floor%20-%20Bobby%20Richards.mp3?alt=media&token=4fe09d01-c064-440e-9fa7-e02005ebd79f',
+    },
+    {
+      id: 2,
+      title: 'Awful',
+      artist: 'josh pan',
+      artwork: require('../assets/img/img2.jpg'),
+      url: 'https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FAudio%2FAwful%20-%20josh%20pan.mp3?alt=media&token=5b174d4c-be09-417c-9fb8-b384f3ce0ec2',
+    },
+    {
+      id: 3,
+      title: 'Something is Going On',
+      artist: 'Godmode',
+      artwork: require('../assets/img/img3.jpg'),
+      url: 'https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FAudio%2FSomething%20is%20Going%20On%20-%20Godmode.mp3?alt=media&token=ecf0d5c5-bc93-48c3-9046-077638d12cfd',
+    },
+    {
+      id: 4,
+      title: 'Book The Rental Wit It',
+      artist: 'RAGE',
+      artwork: require('../assets/img/img4.jpg'),
+      url: 'https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FAudio%2FBook%20The%20Rental%20Wit%20It%20-%20RAGE.mp3?alt=media&token=6f76a691-fd9c-4057-ac0a-0e39104e865e',
+    },
+    {
+      id: 5,
+      title: 'Crimson Fly',
+      artist: 'Huma-Huma',
+      artwork: require('../assets/img/img5.jpg'),
+      url: 'https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FAudio%2FCrimson%20Fly%20-%20Huma-Huma.mp3?alt=media&token=b2d30b27-286e-4d7d-82ad-1bdfa76a4058',
+    },
+  ];
+  const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
 
-  //   changing the track on complete
+
+ 
+ const fetchProfilePicturesRequest = async () => {
+    const reference = await storage()
+    .ref(`files`)
+    .listAll();
+    
+    var imageHolder = [];
+    
+    for(let i=0;i<reference.items.length;i++){
+       await reference.items[i].getDownloadURL().then((urlValues) => {
+          imageHolder.push(urlValues);
+       });
+    }
+  
+    return imageHolder;
+    };
+    const setupsong = async () => {
+      const songs_1=await fetchProfilePicturesRequest();
+      const arr_song=[]
+      for(var i=0; i< songs_1.length;i++){
+        const t= {
+          id: i,
+          title: '19th Floor',
+          artist: 'trak'+i,
+          artwork: require('../assets/img/img1.jpg'),
+          url:songs_1[i],
+        }
+        arr_song.push(t)
+      }
+  
+      setElems(arr_song)
+      
+    }
+  const setupPlayer = async () => {
+
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.updateOptions({
+        capabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.SkipToNext,
+          Capability.SkipToPrevious,
+          Capability.Stop,
+        ],
+      });
+      await TrackPlayer.add(elems);
+    } catch (error) {
+      console.log("rata=",error);
+    }
+  };
+  
+  const togglePlayBack = async playBackState => {
+    const currentTrack = await TrackPlayer.getCurrentTrack();
+    console.log(currentTrack, playBackState, State.Playing);
+    if (currentTrack != null) {
+      if (playBackState == State.Paused) {
+        await TrackPlayer.play();
+      } else {
+        await TrackPlayer.pause();
+      }
+    }
+  };
+  
+ 
   useTrackPlayerEvents([Event.PlaybackTrackChanged], async event => {
     if (event.type === Event.PlaybackTrackChanged && event.nextTrack !== null) {
       const track = await TrackPlayer.getTrack(event.nextTrack);
@@ -117,6 +203,9 @@ const MusicPlayer = () => {
   };
 
   useEffect(() => {
+ 
+    console.log("sogs=",elems)
+    setupsong();
     setupPlayer();
 
     scrollX.addListener(({value}) => {
@@ -147,7 +236,9 @@ const MusicPlayer = () => {
     });
   };
 
+
   const renderSongs = ({item, index}) => {
+    
     return (
       <Animated.View style={style.mainWrapper}>
         <View style={[style.imageWrapper, style.elevation]}>
@@ -164,13 +255,23 @@ const MusicPlayer = () => {
   return (
     <SafeAreaView style={style.container}>
       {/* music player section */}
+      {/* <ScrollView
+           refreshControl={
+              <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              
+              />
+            }>
+        </ScrollView> */}
       <View style={style.mainContainer}>
         {/* Image */}
+      
 
         <Animated.FlatList
           ref={songSlider}
           renderItem={renderSongs}
-          data={songs}
+          data={elems}
           keyExtractor={item => item.id}
           horizontal
           pagingEnabled
